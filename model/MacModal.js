8a6672109595925e338aa44af8a400f1f8b06fec
Darshit Tarapara <sepl.darshittarapara@gmail.com>
2024-07-22 17:02:35 +0530

    Add console to debugging

--- model/MacModal.js
+++ model/MacModal.js
@@ -2,6 +2,7 @@ const os = require("os");
 class MacAddress {
   getIPAddress() {
     const nets = os.networkInterfaces();
+    console.log('nets', nets)
     const address = nets?.["Wi-Fi"]?.[1]?.address;
     const mac = nets?.["Wi-Fi"]?.[1]?.mac;
     return {
