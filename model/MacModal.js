a4e818c3a0c17f5f3545223f98754335ae8b706f
Darshit Tarapara <sepl.darshittarapara@gmail.com>
2024-07-22 17:44:30 +0530

    Change library to fix network interface issue

--- model/MacModal.js
+++ model/MacModal.js
@@ -1,13 +1,11 @@
-const os = require("os");
+const address = require("address");
 class MacAddress {
   getIPAddress() {
-    const nets = os.networkInterfaces();
-    console.log('nets', nets)
-    const address = nets?.["Wi-Fi"]?.[1]?.address;
-    const mac = nets?.["Wi-Fi"]?.[1]?.mac;
+    const response = address.getInterfaceAddress('', 'Wi-Fi');
+    console.log('response', response)
     return {
-      ip: address,
-      mac,
+      ip: response.address,
+      mac:response.mac,
     };
   }
 }
