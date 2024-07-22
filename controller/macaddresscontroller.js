c9a7e3d5fc39e91582efc1622faa44af4733f83a
Darshit Tarapara <sepl.darshittarapara@gmail.com>
2024-07-22 16:35:06 +0530

    Formatted a code

--- controller/macaddresscontroller.js
+++ controller/macaddresscontroller.js
@@ -2,23 +2,22 @@ const MacAddress = require('../model/MacModal');
 
 exports.getMacAddress = async (req, res) => {
     const data = await new MacAddress().getMacAddress()
-console.log('data', data)
-Object.entries(data).forEach(([key, value]) => {
-    console.log(key, value)
-    if (data[key].mac) {
-        res.status(200).json({
-            mac: value?.mac,
-            status: true,
-            ip: value?.ipv4,
-            message: "Data fetch succssfully"
-        })
+    console.log('data', data)
+    Object.entries(data).forEach(([key, value]) => {
+        if (data[key].mac) {
+            res.status(200).json({
+                mac: value?.mac,
+                status: true,
+                ip: value?.ipv4,
+                message: "Data fetch succssfully"
+            })
 
-        return
-    }
-    res.status(500).json({
-        status: false,
-        message: "Something went wrong"
+            return
+        }
+        res.status(500).json({
+            status: false,
+            message: "Something went wrong"
+        })
     })
-})
-   
+
 }
